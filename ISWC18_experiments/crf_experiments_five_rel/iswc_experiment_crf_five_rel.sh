b868b9a24a06dd22294382454d43ae92a14211fe
paramitamirza <paramita135@gmail.com>
2018-09-10 22:42:24 +0200

    update ISWC18 experimental files

--- ISWC18_experiments/crf_experiments_five_rel/iswc_experiment_crf_five_rel.sh
+++ ISWC18_experiments/crf_experiments_five_rel/iswc_experiment_crf_five_rel.sh
@@ -5,3 +5,8 @@ sh iswc_experiment_crf_per_relation.sh P150 1.0 0.2
 sh iswc_experiment_crf_per_relation.sh P527_Q2088357 0.5 0.1
 sh iswc_experiment_crf_per_relation.sh P40 0.5 0.1
 sh iswc_experiment_crf_per_relation.sh P26 0.5 0.1
+
+sh iswc_experiment_crf_per_relation_negation.sh P40 0.5 0.1
+sh iswc_experiment_crf_per_relation_negation.sh P26 0.5 0.1
+
+
