bcc282cd30698a1479687d15141d0f24e29ffc12
paramitamirza <paramita135@gmail.com>
2018-09-11 19:27:10 +0200

    Update ISWC18 experimental files

--- ISWC18_experiments/crf_experiments_five_rel/iswc_experiment_crf_five_rel.sh
+++ ISWC18_experiments/crf_experiments_five_rel/iswc_experiment_crf_five_rel.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#Five relations: containsWork (P527_Q7725310), containsAdmin (P150), hasMember (P527_Q2088357), hasChild (P40), P26 (hasSpouse)
-sh iswc_experiment_crf_per_relation.sh P527_Q7725310 1.0 0.1
-sh iswc_experiment_crf_per_relation.sh P150 1.0 0.2
-sh iswc_experiment_crf_per_relation.sh P527_Q2088357 0.5 0.1
-sh iswc_experiment_crf_per_relation.sh P40 0.5 0.1
-sh iswc_experiment_crf_per_relation.sh P26 0.5 0.1
+#Five relations: containsWork (P527_Q7725310), containsAdmin (P150), hasMember (P527_Q2088357), 
+#hasChild (P40) and hasSpouse (P26)
+bash iswc_experiment_crf_per_relation.sh P527_Q7725310 1.0 0.1
+bash iswc_experiment_crf_per_relation.sh P150 1.0 0.2
+bash iswc_experiment_crf_per_relation.sh P527_Q2088357 0.5 0.1
+bash iswc_experiment_crf_per_relation.sh P40 0.5 0.1
+bash iswc_experiment_crf_per_relation.sh P26 0.5 0.1
 
-#Zero counting quantifier (negation): hasChild (P40), P26 (hasSpouse)
-sh iswc_experiment_crf_per_relation_negation.sh P40 0.5 0.1
-sh iswc_experiment_crf_per_relation_negation.sh P26 0.5 0.1
+#Zero counting quantifier (negation): hasChild (P40) and hasSpouse (P26)
+bash iswc_experiment_crf_per_relation_negation.sh P40 0.5 0.1
+bash iswc_experiment_crf_per_relation_negation.sh P26 0.5 0.1
 
 
